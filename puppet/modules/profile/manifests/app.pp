d6bd1cf89e29234e1c0cfdcf45e6956c40488647
Romain Niveau <romain.niveau@gmail.com>
2016-12-13 17:19:07 +0100

    Finish puppet script

--- puppet/modules/profile/manifests/app.pp
+++ puppet/modules/profile/manifests/app.pp
@@ -2,6 +2,15 @@ class profile::app {
 
   $application_port = hiera('application_port')
 
+  yumrepo { "node-repo":
+    baseurl => "https://rpm.nodesource.com/pub_6.x/el/7/x86_64/",
+    enabled  => 1,
+    gpgcheck => 0
+  } ->
+  package { 'nodejs':
+    ensure => 'latest'
+  }
+
   yumrepo { "xebia-repo":
     baseurl  => 'http://repo.docker',
     descr    => "xebia repo",
@@ -13,9 +22,14 @@ class profile::app {
     ensure => 'latest'
   } ->
 
-  file {'/opt/app/config.json' :
-    ensure => 'present',
+  file { '/opt/app/config.json' :
+    ensure  => 'present',
     content => template('profile/app/config.json.erb')
+  } ->
+
+  service { 'xebia-app':
+    ensure => 'running',
+    enable => true
   }
 
 
